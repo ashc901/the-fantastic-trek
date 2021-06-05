39bb2d4374b956b68822c96d9c68fbbe3cba4e86
ashc901 <ac.carpenter193@gmail.com>
2021-06-05 18:29:31 -0500

    Add notes on create

---
+++ src/components/CreateWalk/CreateWalkV2.js
@@ -0,0 +1,18 @@
+// file for when you have actual walks planned out for the user
+// this has just become an organize thoughts page whoops
+
+//  CREATE Wants:
+// - a dropdown menu
+// - items to select
+// - specific miles assigned to each of these options
+// - then submit
+// this create page should not be too difficult
+
+// UPDATE Wants:
+// - should be mostly the same as before
+// - will update only the distance walked by the user
+// - the walk will not be continued after the <goal> is reached
+
+// VISUAL Wants:
+// - A map
+// - lines on the map correlated to the route picked
